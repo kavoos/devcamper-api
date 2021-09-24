b3ecdbbeecef3070dbb2ba14a1672ec6230cfb1a
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-24 14:45:05 +0200

    Add morgan and use it as logger middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // route files
 const bootcamps = require('./routes/bootcamps');
@@ -9,6 +10,11 @@ dotenv.config({ path: './config/config.env' });
 
 // initialize the application
 const app = express();
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // mount routers
 app.use('/api/v1/bootcamps', bootcamps);
