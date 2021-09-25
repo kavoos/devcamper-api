b0ebed344d8267426887aea72fb01f667378b906
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-25 12:39:35 +0200

    Create Bootcamp POST

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const bootcamps = require('./routes/bootcamps');
 // initialize the application
 const app = express();
 
+// body parser
+app.use(express.json());
+
 // dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
