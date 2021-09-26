9654a47f53f006db82ab8df0227a82bfb6362557
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-26 21:44:28 +0200

    Add Course and modify the seeder

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI);
@@ -16,11 +17,15 @@ mongoose.connect(process.env.MONGO_URI);
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // import into database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log('Data imported'.green.inverse);
     process.exit(1);
@@ -33,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log('Data destroyed'.red.inverse);
     process.exit(1);
