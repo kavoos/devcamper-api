1bc1f9b415d9cf7dd691cf177eea3e7638efca1d
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-26 08:02:25 +0200

    Get bootcamps within a radius

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse');
+const geocoder = require('../utils/geocoder');
 const asyncHandler = require('../middlewares/async');
 const Bootcamp = require('../models/Bootcamp');
 
@@ -67,7 +68,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 // @desc    delete bootcamp
-// @route   DELETe api/vi/bootcamps/:id
+// @route   DELETE api/vi/bootcamps/:id
 // @access  DELETE
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
@@ -81,5 +82,32 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   res.status(201).json({
     success: true,
     data: { name: bootcamp.name },
+  });
+});
+
+// @desc    get bootcamps within a radius
+// @route   GET api/vi/bootcamps/radius/:zipcode/:distance
+// @access  DELETE
+exports.getBootcampsWithinRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // get latitude/longitude from the zipcode
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // calculate radius using radians by dividing distance by radius of the earth
+  // radius of earth is 3,963mi or 6,378km
+  const EARTH_RADIUS = 3963;
+  const radius = distance / EARTH_RADIUS;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
   });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,15 +5,19 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsWithinRadius,
 } = require('../controllers/bootcamps');
 
 const router = express.Router();
 
 router.route('/').get(getBootcamps).post(createBootcamp);
+
 router
   .route('/:id')
   .get(getBootcamp)
   .put(updateBootcamp)
   .delete(deleteBootcamp);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsWithinRadius);
 
 module.exports = router;
