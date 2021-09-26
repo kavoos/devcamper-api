fdda1d318cd65d416fe4ae7a76bb200acf5ba66f
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-26 07:22:52 +0200

    Seed database for Bootcamp from a file

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// load env variables
+dotenv.config({ path: './config/config.env' });
+
+// load models
+const Bootcamp = require('./models/Bootcamp');
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI);
+
+// read the json files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// import into database
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log('Data imported'.green.inverse);
+    process.exit(1);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// delete from database
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log('Data destroyed'.red.inverse);
+    process.exit(1);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// use `node seeder -i` to import data
+// use `node seeder -d` to delete data
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
