f47e87963d37a42a789fb8ac77b7c13cbac52cd7
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-29 17:17:38 +0200

    Add get course by id

--- controllers/courses.js
+++ controllers/courses.js
@@ -26,3 +26,24 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     data: courses,
   });
 });
+
+// @desc    Get single
+// @route   GET api/vi/courses/:id
+// @access  Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
