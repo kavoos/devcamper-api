67063cfae5e46c6c640ed013fd8c0ccc4b398439
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-10-03 20:16:22 +0200

    Add logout and clear cookie after that

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,11 +11,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // set token from bearer token in header
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    // set token from cookie
+    token = req.cookies.token;
   }
-  // else (req.cookies.token) {
-  //     token = req.cookies.token
-  // }
 
   // make sure token exists
   if (!token) {
